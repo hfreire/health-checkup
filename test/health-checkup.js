4926e381116ba66141c5541d20e78f7faca5b61f
Hugo Freire <hfreire@exec.sh>
2016-10-30 15:03:41 +0100

    Added one more test.

--- test/health-checkup.js
+++ test/health-checkup.js
@@ -8,23 +8,62 @@
 const Promise = require('bluebird')
 const test = require('blue-tape')
 
-test('run single health check', (t) => {
-  t.plan(3)
+test('should run health checkup with a healthy and unhealthy check', (t) => {
+  t.plan(8)
 
   const Health = require('../src/health-checkup')
 
-  const check = () => new Promise((resolve) => {
-    t.pass('ran health check')
+  const checks = []
+  checks.push({
+    name: 'healthy check',
+    check: () => new Promise((resolve) => {
+      t.pass('ran healthy check')
 
-    return resolve()
+      return resolve()
+    })
+  })
+  checks.push({
+    name: 'unhealthy check',
+    check: () => new Promise((resolve, reject) => {
+      t.pass('ran unhealthy check')
+
+      return reject(new Error('unhealthy by nature'))
+    })
   })
 
-  Health.addCheck('check1', check)
+  checks.forEach(({ name, check }) => Health.addCheck(name, check))
 
   return Health.checkup()
     .then(report => {
-      t.equal(report.length, 1, 'health report has only 1 check')
-      t.equal(report[ 0 ].name, 'check1', 'check has correct name')
+      t.equal(report.length, 2, 'health report has only 1 check')
+      t.equal(report[ 0 ].name, 'healthy check', 'healthy check has correct name')
+      t.equal(report[ 0 ].is_healthy, true, 'healthy check has correct health')
+      t.equal(report[ 1 ].name, 'unhealthy check', 'healthy check has correct name')
+      t.equal(report[ 1 ].is_healthy, false, 'unhealthy check has correct health')
+      t.equal(report[ 1 ].reason, 'unhealthy by nature', 'unhealthy check has correct reason')
     })
+})
+
+test('should fail adding check when using invalid parameters', (t) => {
+  t.plan(2)
+
+  const Health = require('../src/health-checkup')
+
+  const name = 'check'
+  const check = () => Promise.resolve()
+
+  try {
+    Health.addCheck(name, null)
+  } catch (error) {
+    t.pass('failed when adding invalid check function')
+  }
+
+  try {
+    Health.addCheck(null, check)
+  } catch (error) {
+    t.pass('failed when adding invalid check name')
+  }
+
+  t.end()
 })
 
